d7b7960619b0dade149258e6076f3609b831133c
haidera123 <58245971+haidera123@users.noreply.github.com>
2023-07-10 06:06:06 -0700

    Update Divorce settlement script.js

--- Divorce settlement script.js
+++ Divorce settlement script.js
@@ -40,12 +40,14 @@ function next() {
   checkForFirstSection();
   hideAllQuestions();
   document.getElementById(arrayQues[counter]).style.display = "block";
+  scrollToDiv()
 }
 function previous() {
   counter--;
   checkForFirstSection();
   hideAllQuestions();
   document.getElementById(arrayQues[counter]).style.display = "block";
+  scrollToDiv()
 }
 
 function hideAllQuestions() {
@@ -512,3 +514,7 @@ var formatter = new Intl.NumberFormat("en-US", {
   minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
   maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
 });
+function scrollToDiv() {
+  var element = document.getElementById("inputContainerCalc");
+  element.scrollIntoView({ behavior: "smooth" });
+}
